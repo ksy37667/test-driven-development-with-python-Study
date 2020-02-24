ea087deafca3e4f1adc2fb490708f4bdb19a1c88
LeeHyeongKon <ksy37667@gmail.com>
2020-02-25 08:57:21 +0900

    기능테스트가 작업 아이템을 추가할 수 있는지 확인

--- tdd/function_tests.py
+++ tdd/function_tests.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
 
@@ -6,7 +7,7 @@ class NewvisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
     def tearDown(self):
-        pass
+        self.browser.quit()
         
     
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -16,18 +17,35 @@ class NewvisitorTest(unittest.TestCase):
 
         # 웹 페이지 타이틀과 헤더가 'To-Do' 를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').next
+        self.assertIn('To-Do', header_text)
 
         # 바로 작업을 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get.attribute('placeholder'),
+            '작업 아이템 입력'
+        )
 
         # "공작깃털 사기"라고 텍스트 상자에 입력한다
         # (취미는 날치 잡이용 그물을 만드는 것)
+        inputbox.send_keys('공작깃털 사기')
+
 
         # 엔터키를 누르면 페이지가 생긴되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Key.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
         # 다시 "공작깃털을 이용해서 그물 만들기" 라고 입력한다
+        self.fail('Finish the test!')
+
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
