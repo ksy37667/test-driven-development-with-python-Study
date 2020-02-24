2a61e3516c29247a4c095796ee7813c4f4a8d984
LeeHyeongKon <ksy37667@gmail.com>
2020-02-25 08:41:56 +0900

    최소 html을 반환하는 기본 뷰

--- tdd/function_tests.py
+++ tdd/function_tests.py
@@ -6,12 +6,13 @@ class NewvisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
     def tearDown(self):
-        self.browser.quit()
+        pass
+        
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://127.0.0.1:8000')
 
         # 웹 페이지 타이틀과 헤더가 'To-Do' 를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
