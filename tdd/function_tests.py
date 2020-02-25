f23ce5a13d834ce1cde4dcef467d82c07c7b41f7
LeeHyeongKon <ksy37667@gmail.com>
2020-02-25 09:20:05 +0900

    템플릿을 이용한 메인 페이지 HTML 생성

--- tdd/function_tests.py
+++ tdd/function_tests.py
@@ -17,13 +17,13 @@ class NewvisitorTest(unittest.TestCase):
 
         # 웹 페이지 타이틀과 헤더가 'To-Do' 를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').next
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # 바로 작업을 추가하기로 한다
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get.attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             '작업 아이템 입력'
         )
 
@@ -34,12 +34,13 @@ class NewvisitorTest(unittest.TestCase):
 
         # 엔터키를 누르면 페이지가 생긴되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
-        inputbox.send_keys(Key.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다."
         )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
